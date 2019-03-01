40407ffd42e67788c187bd54d8a4b3bcfeda0749
Cristian Radu <cris@Cristians-MacBook-Pro.local>
2019-03-01 20:07:21 +0200

    hello

---
+++ cce_bot.py
@@ -0,0 +1,19 @@
+from __future__ import unicode_literals, print_function
+from prompt_toolkit import print_formatted_text
+from prompt_toolkit.formatted_text import HTML, ANSI, FormattedText
+from prompt_toolkit.styles import Style
+import pandas as pd
+from game import dataLoad
+
+print = print_formatted_text
+
+def main():
+    print(HTML('<orange><b>Combat Commander: Europe</b></orange> - <white>Combat bot</white>'))
+    print(HTML('====================================='))
+    print()
+    print(HTML('Select Scenario:'))
+    print(dataLoad.get_googleSheetRecords())
+
+
+if __name__ == '__main__':
+    main()
---
+++ game/dataLoad.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import pygsheets
+from oauth2client.service_account import ServiceAccountCredentials
+import os.path
+
+
+def get_googleSheetRecords():
+
+    if os.path.isfile('CombatCommanderEurope.xlsx'):
+        print ('xlsx exists!')
+        xls = pd.ExcelFile('CombatCommanderEurope.xlsx')     
+        dataframe_collection = {} 
+        for title in xls.sheet_names:
+           dataframe_collection[title] = xls.parse(title)
+        return dataframe_collection
+    else:
+        # scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+        # credentials = ServiceAccountCredentials.from_json_keyfile_name('SolitaireBoardGames-c207d72eba1a.json', scope)
+        gc = pygsheets.authorize(service_file='SolitaireBoardGames-c207d72eba1a.json')
+        wks = gc.open('CombatCommanderEurope')
+        
+        worksheet_list = wks.worksheets()        
+        dataframe_collection = {} 
+        for item in worksheet_list:
+            current_sheet = wks.worksheet(item.title)
+            print("Downloading: ", current_sheet.title)
+            # Extract all records
+            dataframe_collection[current_sheet.title] = get_as_dataframe(current_sheet)            
+            print("\n")
+        return dataframe_collection
+    
+
+
+
+
