2fff4ea3dca89605bc2361698b4808461e2bbced
Cristian Radu <cristi@Cristians-Mac-mini.local>
2019-03-03 10:50:32 +0200

    corecturi hex

--- game/hex.py
+++ game/hex.py
@@ -1,14 +1,15 @@
 from collections import namedtuple
-import string
+import string,re
 
 OffsetCoord = namedtuple("OffsetCoord", "col, row")
 odd_q = -1
 def literal_to_coord(s):
     ASCII_List = string.ascii_uppercase
-    col = int(s[1])+odd_q
+    col = int(re.search(r'\d+', s).group())+odd_q
     row = ASCII_List.index(s[0])
-    # row = h.r + (h.q + offset * (h.q & 1)) // 2
     return OffsetCoord(col, row)
 
 #test
-print(literal_to_coord('A1'))
+print("A1: ",literal_to_coord('A1'))
+print('B2: ',literal_to_coord('B2'))
+print('N10: ',literal_to_coord('N10'))
